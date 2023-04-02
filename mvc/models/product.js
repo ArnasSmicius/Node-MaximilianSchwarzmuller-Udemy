451202a7fdf5c5a8d41df631d823c030ee2a153f
Henrikas Arnas Smičius <henrikas.smicius@vinted.com>
2023-04-02 19:55:08 +0300

    Storing add product form results into files

--- mvc/models/product.js
+++ mvc/models/product.js
@@ -1,4 +1,11 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
 
 module.exports = class Product {
   constructor(t) {
@@ -6,10 +13,24 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
   }
 };

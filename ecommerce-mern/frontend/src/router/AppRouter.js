d05d6ac09c06ce2c7038cff9c318387c0275bd21
Henrikas Arnas Smičius <henrikas.smicius@vinted.com>
2023-04-05 18:09:00 +0300

    Add 404 page template

--- ecommerce-mern/frontend/src/router/AppRouter.js
+++ ecommerce-mern/frontend/src/router/AppRouter.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "../components/Index/Index";
 import AddProduct from "../components/AddProduct/AddProduct";
+import NoPage from "../components/NoPage/NoPage";
 
 const AppRouter = () => {
   return (
@@ -9,6 +10,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/admin/add-product" element={<AddProduct />} />
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
   );

7671dfde28e7dd3f5903276b578995b6bc49cab3
Henrikas Arnas Smičius <henrikas.smicius@vinted.com>
2023-04-07 12:47:44 +0300

    Implement Add Product page UI

--- ecommerce-mern/frontend/src/components/AddProduct/AddProduct.js
+++ ecommerce-mern/frontend/src/components/AddProduct/AddProduct.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+import styles from "./AddProduct.module.css";
+
 const AddProduct = () => {
-  return <h1>Welcome from Add Product page</h1>;
+  return (
+    <main>
+      <form
+        className={styles["product-form"]}
+        action="/admin/add-product"
+        method="POST"
+      >
+        <div className={styles["form-control"]}>
+          <label for="title">Title</label>
+          <input type="text" name="title" id="title" />
+        </div>
+        <button className={styles.btn} type="submit">
+          Add Product
+        </button>
+      </form>
+    </main>
+  );
 };
 
 export default AddProduct;

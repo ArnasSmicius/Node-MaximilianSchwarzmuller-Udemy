dfe76ebb7198cefba6bcb2701fc89c404f71662d
Henrikas Arnas Smičius <henrikas.smicius@vinted.com>
2023-04-07 14:08:15 +0300

    Add Product CREATE and READ end-points

---
+++ ecommerce-mern/backend/controllers/products.js
@@ -0,0 +1,13 @@
+const Product = require("../models/product");
+
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.send(product);
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.send(products);
+  });
+};
--- ecommerce-mern/backend/index.js
+++ ecommerce-mern/backend/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send({
-    title: "Hello world!",
-  });
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);

d7bd60fb65f7680779df4fcb51b9912cef3e4cc0
Henrikas Arnas Smičius <henrikas.smicius@vinted.com>
2023-04-11 16:18:44 +0300

    Add more routes and components for some pages

--- ecommerce-mern/frontend/src/router/AppRouter.js
+++ ecommerce-mern/frontend/src/router/AppRouter.js
@@ -4,14 +4,22 @@ import ProductList from "../features/shop/product-list/ProductList";
 import AddProduct from "../features/admin/add-product/AddProduct";
 import NoPage from "../features/no-page/NoPage";
 import Navigation from "../features/navigation/Navigation";
+import Shop from "../features/shop/shop/Shop";
+import Cart from "../features/shop/cart/Cart";
+import Checkout from "../features/shop/checkout/Checkout";
+import Products from "../features/admin/products/Products";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Navigation />
       <Routes>
-        <Route path="/" element={<ProductList />} />
+        <Route path="/" element={<Shop />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
         <Route path="/admin/add-product" element={<AddProduct />} />
+        <Route path="/admin/products" element={<Products />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>

bb1f99124de06832b1859745d5fef2810d62b4a7
Henrikas Arnas Smičius <henrikas.smicius@vinted.com>
2023-04-15 11:18:16 +0300

    Add Product Details route

--- ecommerce-mern/frontend/src/router/AppRouter.js
+++ ecommerce-mern/frontend/src/router/AppRouter.js
@@ -9,6 +9,7 @@ import Cart from "../features/shop/cart/Cart";
 import Checkout from "../features/shop/checkout/Checkout";
 import Products from "../features/admin/products/Products";
 import Orders from "../features/shop/orders/Orders";
+import ProductDetails from "../features/shop/product-details/ProductDetails";
 
 const AppRouter = () => {
   return (
@@ -17,6 +18,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Shop />} />
         <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/checkout" element={<Checkout />} />

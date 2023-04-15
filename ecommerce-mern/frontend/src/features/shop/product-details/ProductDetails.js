1f74d01dcec69781168499613b73678f067f8b6b
Henrikas Arnas Smičius <henrikas.smicius@vinted.com>
2023-04-15 17:16:29 +0300

    Implement Product Details UI

--- ecommerce-mern/frontend/src/features/shop/product-details/ProductDetails.js
+++ ecommerce-mern/frontend/src/features/shop/product-details/ProductDetails.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getProduct } from "../../../services/product";
+import styles from "./ProductDetails.module.css";
 import NoPage from "../../no-page/NoPage";
+import Button from "../../../components/Button/Button";
 
 const ProductDetails = (props) => {
   const params = useParams();
@@ -10,15 +12,33 @@ const ProductDetails = (props) => {
 
   useEffect(() => {
     getProduct(params.productId)
-      .then((response) => setProduct(response))
+      .then((response) => {
+        setProduct(response);
+        document.title = response.title;
+      })
       .catch((error) => setPageNotFound(true));
   }, []);
+
+  const content = () => {
+    return (
+      <main className={styles.centered}>
+        <h1>{product.title}</h1>
+        <hr />
+        <div>
+          <img src={product.imageUrl} alt={product.title} />
+        </div>
+        <h2>{product.price}</h2>
+        <p>{product.description}</p>
+        <Button>Add to Cart</Button>
+      </main>
+    );
+  };
 
   return (
     <div>
       {pageNotFound && <NoPage />}
       {pageNotFound === false && product === undefined && <h1>Loading...</h1>}
-      <div>{JSON.stringify(product)}</div>
+      {pageNotFound === false && product != undefined && content()}
     </div>
   );
 };
